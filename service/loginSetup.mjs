13ef38f00dfe74687f5e74328fa81bc28d9af0b5
Lauriano <laurianocarlos@hotmail.com>
2024-11-19 10:18:57 -0300

    fix: Timeout global

--- service/loginSetup.mjs
+++ service/loginSetup.mjs
@@ -1,6 +1,6 @@
 import puppeteer from 'puppeteer';
 import { login } from './login.mjs';
-
+import { TIME } from '../config/constant.mjs';
 let browser;
 let page;
 
@@ -8,6 +8,7 @@ export async function setup() {
   if (!browser) {
     browser = await puppeteer.launch({ headless: false });
     page = await browser.newPage();
+    page.setDefaultTimeout(TIME.ONE_MINUTE);
     await page.setViewport();
     await login(page); 
   }
