22f92d93d3fe7cf25a7c26ec60ce159810644c95
Lauriano <laurianocarlos@hotmail.com>
2024-12-18 16:58:11 -0300

    feat:Ur dataTable

--- tests/conciliation/conciliation.test.mjs
+++ tests/conciliation/conciliation.test.mjs
@@ -1,14 +1,43 @@
 import { expect } from 'chai';
-import { TIME, PROTOCOL_STATUS, SERVICES } from '../../config/constant.mjs';
+import { TIME, PROTOCOL_STATUS, SERVICES, PER_PAGE, ROUTE } from '../../config/constant.mjs';
 import { getAppPage } from "../../service/loginSetup.mjs";
 import { create } from "../../service/conciliationService/create.mjs";
 import protocolLogger from '../../service/ProtocolCSVLogger.js';
 import { mockConciliation } from '../../helpers/mock.js';
 
 import ApiInterfaceService from '../../core/api-de-interface-clientes.js';
+import { DataTableService } from '../../service/DataTableService.mjs';
 
 describe("Conciliation Test", function () {
     this.timeout(TIME.FOUR_MINUTES);
+
+
+    Object.entries(PER_PAGE).forEach(([key, value]) => {
+        it(`Must change the number of records per page: PerPage: ${value}`, async () => {
+            const page = await getAppPage();
+            const { quantity, message } = await DataTableService.perPage(
+                page,
+                value,
+                ROUTE.CONCILIATION_SEARCH_BASE
+            );
+  
+            expect(quantity).to.equal(value);
+            expect(message).to.include(`Exibindo ${value} registros por página`);
+        });
+    });
+  
+    it.skip('Must navigate to next page', async () => {
+        const page = await getAppPage();
+  
+        const isPreviousEnabled = await DataTableService.goToNextPage(page, ROUTE.CONCILIATION_SEARCH_BASE);
+        expect(isPreviousEnabled).to.be.true;
+    });
+  
+    it.skip('Must load a specific page', async () => {
+        const page = await getAppPage();
+        const isLoaded = await DataTableService.goToPage(page, 2, ROUTE.CONCILIATION_SEARCH_BASE);
+        expect(isLoaded).to.be.true;
+    });
 
     it('You must fill in the fields and send it for registration', async () => {
         const page = await getAppPage();
